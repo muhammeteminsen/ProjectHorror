c7a3efeb60025d010ceb7fafa79bf253846a2103
muhammeteminsen <116170392+muhammeteminsen@users.noreply.github.com>
2025-02-26 01:19:08 +0300

    Inspection & Some Fixing Problems

---
+++ Assets/Scripts/Interactions/Inspection.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using UnityEngine;
+
+public class Inspection : MonoBehaviour
+{
+    [SerializeField] private float inspectionSensitivity = 5f;
+    private Camera _camera;
+    private Quaternion currentRotation;
+    private Vector3 currentPosition;
+    private Coroutine currentCoroutine;
+    private Coroutine inspectionCoroutine;
+    private bool _hasCoroutine;
+    private bool _hasCurrent;
+    private void Awake()
+    {
+        _camera = Camera.main;
+        currentRotation = transform.rotation;
+        currentPosition = transform.position;
+    }
+
+    public void GetInspectionCoroutine()
+    {
+        if (currentCoroutine!=null)
+        {
+            StopCoroutine(currentCoroutine);
+        }
+        inspectionCoroutine = StartCoroutine(StartInspection());
+    }
+
+    public void GetInspection(ref bool isInspection)
+    {
+        if (isInspection)
+        {
+            
+            float mouseX = Input.GetAxis("Mouse X")*inspectionSensitivity;
+            float mouseY = Input.GetAxis("Mouse Y")*inspectionSensitivity;
+            Quaternion currentRotationT = transform.rotation;
+            
+            Quaternion horizontalRotation = Quaternion.AngleAxis(mouseX, Vector3.up);
+            Quaternion verticalRotation = Quaternion.AngleAxis(-mouseY, transform.right);
+            
+            transform.rotation = horizontalRotation * currentRotationT * verticalRotation;
+            if (_hasCoroutine) _hasCoroutine = false; 
+        }
+        else
+        {
+            if (_hasCoroutine) return;
+            if (inspectionCoroutine!=null)
+            {
+                StopCoroutine(inspectionCoroutine);  
+            }
+            currentCoroutine = StartCoroutine(StartCurrent());
+            _hasCoroutine = true;
+
+        }
+        
+    }
+    private IEnumerator StartInspection()
+    {
+        Vector3 newPosition = _camera.transform.position + _camera.transform.forward;
+        Vector3 lookRotation = _camera.transform.position - transform.position;
+        Quaternion newRotation = Quaternion.LookRotation(lookRotation,Vector3.up);
+        float elapsedTime = 0;
+        float duration = 1f;
+        while (elapsedTime<duration)
+        {
+            elapsedTime +=Time.deltaTime;
+            float t = elapsedTime / duration;
+            transform.position = Vector3.Lerp(transform.position,newPosition,t);
+            transform.rotation = Quaternion.Lerp(transform.rotation,newRotation,t);
+            yield return null;
+        }
+    }
+
+    private IEnumerator StartCurrent()
+    {
+        float elapsedTime = 0;
+        float duration = 1f;
+        while (elapsedTime<duration)
+        {
+            elapsedTime +=Time.deltaTime;
+            float t = elapsedTime / duration;
+            transform.position = Vector3.Lerp(transform.position,currentPosition,t);
+            transform.rotation = Quaternion.Lerp(transform.rotation,currentRotation,t);
+            yield return null;
+        }
+    }
+    
+    
+}
--- Assets/Scripts/Interactions/Interaction.cs
+++ Assets/Scripts/Interactions/Interaction.cs
@@ -1,36 +1,63 @@
 using UnityEngine;
+
 public class Interaction : MonoBehaviour
 {
-    [SerializeField,Range(1f,50f)] private float interactDistance = 5f;
+    [SerializeField, Range(1f, 50f)] private float interactDistance = 5f;
+    [SerializeField] private float inspectionSensitivity;
+    private PlayerMovement playerMovement;
     private Camera _camera;
     private UIController _uiController;
+    private Rigidbody _playerRb;
+    private bool _isInspection;
+    private bool _hasCurrent;
+    
     private void Awake()
     {
         _camera = Camera.main;
         _uiController = GetComponent<UIController>();
+        playerMovement = FindAnyObjectByType<PlayerMovement>();
+        _playerRb = playerMovement.gameObject.GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         InteractionAnyObject();
     }
-    
+
 
     private void InteractionAnyObject()
     {
         Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         int layerMask = LayerMask.GetMask("Interaction");
-        if (Physics.Raycast(ray,out hit,interactDistance,layerMask))
+        if (Physics.Raycast(ray, out hit, interactDistance, layerMask))
         {
-            
+            InspectionObject(hit);
             _uiController.crossImage.sprite = _uiController.interactCross;
-            Debug.DrawRay(ray.origin,hit.point - ray.origin,Color.magenta);
+            Debug.DrawRay(ray.origin, hit.point - ray.origin, Color.magenta);
         }
         else
         {
             _uiController.crossImage.sprite = _uiController.defaultCross;
-            Debug.DrawRay(ray.origin,ray.direction*interactDistance,Color.black);
+            Debug.DrawRay(ray.origin, ray.direction * interactDistance, Color.black);
+        }
+    }
+    
+    private void InspectionObject(RaycastHit hit)
+    {
+        if (hit.transform.TryGetComponent(out Inspection inspection))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _isInspection = !_isInspection;
+                playerMovement.enabled = !_isInspection;
+                _playerRb.isKinematic = _isInspection;
+                if (_isInspection)
+                {
+                    inspection.GetInspectionCoroutine();
+                }
+            }
+            inspection.GetInspection(ref _isInspection);
         }
     }
 }
