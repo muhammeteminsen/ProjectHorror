9dedd02d1f70e4687eeb583cdca31f0f0e8f85b0
JonPierlartin <erselkinik77@gmail.com>
2025-02-28 20:57:20 +0300

    Main Menu Basics

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
