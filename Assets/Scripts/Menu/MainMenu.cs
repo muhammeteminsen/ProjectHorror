022c13f298df0f1564dee8fa74f01dd12fc69973
muhammeteminsen <116170392+muhammeteminsen@users.noreply.github.com>
2025-03-02 18:12:01 +0300

    .

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -3,7 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
